acd8ba198b1158c6d1812b42c0366afb1b2c103a
Joaquin Moreno <tec.ignaciomoreno@gmail.com>
2020-01-10 13:34:56 -0300

    nav function

--- design/main.js
+++ design/main.js
@@ -1,4 +1,14 @@
+const itemsOpen = false
 toggler = () =>{
     const toggle = document.getElementById("container")
     toggle.classList.toggle("active")
+    if(this.itemsOpen){
+        this.itemsActive();
+    }
+}
+
+itemsActive = () =>{
+    const toggle = document.getElementById("itemsProjects")
+    toggle.classList.toggle("items-active")
+    this.itemsOpen = !this.itemsOpen
 }
